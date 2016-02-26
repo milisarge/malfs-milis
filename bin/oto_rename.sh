2645a69a6d7bec24d98423e6601bd11fd9a1da65
shamil <shamil@root>
2016-02-26 03:41:51 +0200

    malfs mekanizma guncelleme

--- bin/oto_rename.sh
+++ bin/oto_rename.sh
@@ -1,3 +1,8 @@
 #test sonuclarını gor uygunsa -n i kaldır komutu ver.
 shopt -s globstar
 rename  -n 's/Pkgfile/talimat/' **
+rename  -n 's/post-install/kur-kos/' **
+rename  -n 's/pre-install/kos-kur/' **
+rename  -n 's/.README/.okubeni/' **
+rename  -n 's/-kos-kur/.kos-kur/' **
+rename  -n 's/-kur-kos/.kur-kos/' **
