5ea0cc1680d2a8320aca36705f077f4b3f85367a
root <root@lokal>
2016-04-21 09:03:26 +0300

    chroot

--- bin/otopostpre.sh
+++ bin/otopostpre.sh
@@ -1,4 +1,3 @@
 for i in /tmp/*.POST; do bash $i; done
 rm /tmp/*.POST
 rm /tmp/*.PRE
-mps -Ggit
--- chroot-yapici.sh
+++ chroot-yapici.sh
@@ -1,6 +1,7 @@
 #malf-milis in klonladığınız dizine giriniz.
 #mps-bootstrap da gerekli ikili paket sunucu ayarını yapınız.
 #./chroot-yapici çalıştırın.
+tarih=`date +%Y-%m-%d`
 export LFS=/mnt/lfs
 if [ -d /mnt/lfs ];then
 	rm -r /mnt/lfs
@@ -10,4 +11,4 @@ fi
 ./mps-bootstrap -psk talimatname/temel/derleme.sira /mnt/lfs/
 ./mps-bootstrap -ik git -kok /mnt/lfs
 chroot "/mnt/lfs" sh -c "/root/bin/otopostpre.sh"
-mksquashfs /mnt/lfs /mnt/milis_bootstrap.sfs -comp xz
+mksquashfs /mnt/lfs /mnt/milis-bootstrap$tarih.sfs -comp xz
