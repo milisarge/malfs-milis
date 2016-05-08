e8684f41ea53fac6d0186d8d4b3f39b28a6f9f50
milisman <milisarge@gmail.com>
2016-05-08 08:46:36 +0000

    git-changelog_eklendi

---
+++ changelog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Generates changelog day by day
+NEXT=$(date +%F)
+echo "CHANGELOG"
+echo ----------------------
+git log --no-merges --format="%cd" --date=short | sort -u -r | while read DATE ; do
+    echo
+    echo [$DATE]
+    GIT_PAGER=cat git log --no-merges --format=" * %s" --since=$DATE --until=$NEXT
+    NEXT=$DATE
+done
