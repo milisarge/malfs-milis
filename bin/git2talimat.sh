f109013ea840916989128408cb4c1a629bc7130a
milisman <milisarge@gmail.com>
2016-05-27 20:34:53 +0300

    git2talimat

--- bin/git2talimat.sh
+++ bin/git2talimat.sh
@@ -1,11 +1,32 @@
 packager="milisarge"
 account="$1"
 name="$2"
+version="$3"
 file="$name.html"
 url=https://github.com/"$account/$name"
+
 curl -k $url > "$file" 
 desc=`sed -n 's|[^<]*<span itemprop="about">\([^<]*\)</span>[^<]*|\1\n|gp' $file`
-echo "# description:$desc"
-echo "# url:$url"
-echo "# packager:$packager"
-echo "# depends on: "
+mkdir -p $name
+
+tee "$name/talimat" > /dev/null <<EOF
+# description:$desc
+# url:$url
+# packager:$packager
+# depends on: 
+
+name=$name
+version=$version
+release=1
+source=(https://github.com/$account/$name/archive/$version.tar.gz)
+
+build () {
+	mv $DERLEME_KAYNAKDIZIN/$version.tar.gz $DERLEME_KAYNAKDIZIN/$name-$version.tar.gz
+	cd $name-$version
+	./autogen.sh
+	./configure --prefix=/usr
+	make
+	make DESTDIR=$PKG install
+}
+
+EOF
