d889fd9747b7d8d74198610e6ce6d68fa39b8335
milisman <milisarge@gmail.com>
2016-10-11 23:51:03 +0300

    milis-kurulum-test.py

--- bin/milis-kurulum-test.py
+++ bin/milis-kurulum-test.py
@@ -10,7 +10,9 @@ import crypt
 
 os.system("mps -GG")
 if os.path.exists("/usr/bin/pip3") is False:
-	os.system("pip_kur && pip3 install pythondialog")
+	os.system("mps -G")
+	time.sleep(3)
+	os.system("mps kur python3-pip && pip3 install pythondialog")
 if os.path.exists("/usr/bin/acp") is False:
 	os.system("mps -G")
 	time.sleep(3)
