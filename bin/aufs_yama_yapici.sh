0dab900f40f999d000496af46e1d9351e2c03873
milisman <milisarge@gmail.com>
2016-10-23 23:50:38 +0300

    initramfs_yapici

--- bin/aufs_yama_yapici.sh
+++ bin/aufs_yama_yapici.sh
@@ -5,7 +5,7 @@ rm -Rf aufs4-standalone
 git clone git://github.com/sfjro/aufs4-standalone.git aufs4-standalone
 cd aufs4-standalone
 git branch -r
-git checkout origin/aufs4.4
+git checkout origin/aufs4.7
 
 # modify what you want
 
@@ -13,18 +13,18 @@ git checkout origin/aufs4.4
 # create the patch with the directories: fs, include and Documentation
 
 rm -v $(find . -type f -name '*.orig')
-#grep -qse 'EXPORT_SYMBOL(' aufs4-standalone.patch && \
-#sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs4-standalone.patch
-rm -rf ../tmp/linux-4.4
-mkdir -p ../tmp/linux-4.4
-cp -a fs ../tmp/linux-4.4
-cp -a include ../tmp/linux-4.4
-cp -a Documentation ../tmp/linux-4.4
-rm ../tmp/linux-4.4/include/uapi/linux/Kbuild
+grep -qse 'EXPORT_SYMBOL(' aufs4-standalone.patch && \
+sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs4-standalone.patch
+rm -rf ../tmp/linux-4.7
+mkdir -p ../tmp/linux-4.7
+cp -a fs ../tmp/linux-4.7
+cp -a include ../tmp/linux-4.7
+cp -a Documentation ../tmp/linux-4.7
+rm ../tmp/linux-4.7/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-4.4  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.4-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.4/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' | \
-bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.4/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-4.7  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.7-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.7/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.7/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
--- bin/initramfs_yap.sh
+++ bin/initramfs_yap.sh
@@ -1,1 +1,3 @@
+cd /boot
+ln -s kernel-4.4.27 kernel
 dracut -N --force --xz --omit systemd  /boot/initramfs `uname -r`
