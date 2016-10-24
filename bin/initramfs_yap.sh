dca1545bd5006d94d70bd48f9e1d9be447862cda
milisman <milisarge@gmail.com>
2016-10-24 23:56:28 +0300

    initramfs_yapici

--- bin/initramfs_yap.sh
+++ bin/initramfs_yap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -f /boot/initramfs ];then
+	mv /boot/initramfs /boot/initramfs_eski
+fi
 yenimodul=`ls -Art /lib/modules/ | tail -n 1`
 modulno=`basename $yenimodul`
 dracut -N --force --xz --omit systemd  /boot/initramfs $modulno
