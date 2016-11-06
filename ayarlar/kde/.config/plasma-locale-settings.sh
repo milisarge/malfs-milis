b22132cd83dd0c38a4ba356b6a782e450c427890
giresun28 <alihan-ozturk28@hotmail.com>
2016-11-06 12:24:09 +0200

    kde-ayarlar.eklendi

---
+++ ayarlar/kde/.config/plasma-locale-settings.sh
@@ -0,0 +1,6 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=tr_TR.UTF-8
+export LANGUAGE=tr
