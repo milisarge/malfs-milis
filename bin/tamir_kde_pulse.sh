b98ae72b70257a321dac2b1a313a95f8fd712a27
milisman <milisarge@gmail.com>
2016-12-24 00:46:07 +0200

    tamir-kde-pulse

---
+++ bin/tamir_kde_pulse.sh
@@ -0,0 +1,7 @@
+mps -GG
+if [ ! -f /root/.config/autostart-scripts/pulse.sh ];then
+	cp /root/ayarlar/kde/.config/autostart-scripts/pulse.sh /root/.config/autostart-scripts/pulse.sh
+	echo "gerekli pulse.sh dosyasi olusturuldu."
+else
+	echo "gerekli pulse.sh dosyasi mevcut."
+fi
