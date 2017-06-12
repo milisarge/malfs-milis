daf21b2b9e2ca66eda33bbf8f6b964c4e4c6f23f
milisarge <milisarge@gmail.com>
2017-06-12 05:37:39 +0300

    initramfs

--- bin/initramfs_yap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -f /boot/initramfs ];then
-	mv /boot/initramfs /boot/initramfs_eski
-fi
-yenimodul=`ls -Art /lib/modules/ | tail -n 1`
-modulno=`basename $yenimodul`
-dracut -N --force --xz --omit systemd  /boot/initramfs $modulno
