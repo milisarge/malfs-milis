2a4ff3e2e9d9e7c6b09575172f4e2a7e9cfd80b0
milisbir <milisarge@gmail.com>
2017-10-22 07:22:54 +0300

    chromium.paketlendi

--- talimatname/genel/chromium/chromium.sh
+++ talimatname/genel/chromium/chromium.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Override command-line options
+# Allow users to override command-line options
 # Based on Gentoo's chromium package (and by extension, Debian's)
-if [[ -f /etc/chromium/default ]]; then
-	. /etc/chromium/default
-fi
+for FILE in /etc/chromium/*.conf ; do
+  [[ -f ${FILE} ]] && source "${FILE}"
+done
 
-# Prefer user defined CHROMIUM_USER_FLAGS (from env) over system
-# default CHROMIUM_FLAGS (from /etc/chromium/default)
+# Prefer user defined CHROMIUM_USER_FLAGS flags (from environment) over
+# system default CHROMIUM_FLAGS (from /etc/chromium)/)
 CHROMIUM_FLAGS=${CHROMIUM_USER_FLAGS:-$CHROMIUM_FLAGS}
-
 
 export CHROME_WRAPPER=$(readlink -f "$0")
 export CHROME_DESKTOP=chromium.desktop
 
-exec /opt/chromium/chromium $CHROMIUM_FLAGS "$@"
-
+exec /usr/lib@LIBDIRSUFFIX@/chromium/chromium $CHROMIUM_FLAGS "$@"
