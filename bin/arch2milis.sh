9c7b9905abc39e35f059aac54e6c9c71408a5c7a
milisbir <milisarge@gmail.com>
2017-11-06 05:05:17 +0200

    duzenleme

--- bin/arch2milis.sh
+++ bin/arch2milis.sh
@@ -4,10 +4,10 @@
 if [ -z "$1" ]; then
     echo "kullanım: arch2milis.sh  talimat_yol"
 else
-	sed -i 's/pkgname/name/g' $1
-	sed -i 's/pkgver/version/g' $1
-	sed -i 's/pkgrel/release/g' $1
-	sed -i 's/pkgdesc/# Description/g' $1
+	sed -i 's/pkgname/isim/g' $1
+	sed -i 's/pkgver/surum/g' $1
+	sed -i 's/pkgrel/devir/g' $1
+	sed -i 's/pkgdesc/# Tanım/g' $1
 	sed -i 's/{pkgdir}/{PKG}/g' $1
 	sed -i 's/pkgdir/PKG/g' $1
 	sed -i 's/{srcdir}/{SRC}/g' $1
