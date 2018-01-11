a43ff67f70361ee5fa01064224e35bd3dfdc132d
milisbir <milisarge@gmail.com>
2018-01-11 17:55:34 +0200

    intel-ucode_guncelle

--- bin/intel-ucode_guncelle.sh
+++ bin/intel-ucode_guncelle.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-echo "yapım aşamasında"
-exit 1
 mps guncelle
-[ ! -d /var/lib/pkg/DB/iucode-tool ] && mps kur iucode-tool
+#[ ! -d /var/lib/pkg/DB/iucode-tool ] && mps kur iucode-tool
 [ "`mps bil surum intel-ucode`" <> "2.1.15" ] && mps -g intel-ucode
 echo "early_microcode=yes" >>  /usr/lib/dracut/dracut.conf.d/intel_ucode.conf
-iucode_tool -S --write-earlyfw=/boot/guncel_ucode.cpio /lib/firmware/intel-ucode/*
+# buna gerek yok dracut oto dahil ediyor.
+#iucode_tool -S --write-earlyfw=/boot/guncel_ucode.cpio /lib/firmware/intel-ucode/*
 initramfs_guncelle
 grub-guncelle
