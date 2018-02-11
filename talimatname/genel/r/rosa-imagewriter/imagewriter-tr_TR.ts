9a558cdfef6b7c66a9553f46988f32ad52d9d9bf
milisbir <milisarge@gmail.com>
2018-02-11 23:51:49 +0200

    rosaimage-writer

---
+++ talimatname/genel/r/rosa-imagewriter/imagewriter-tr_TR.ts
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <source>b</source>
+        <translation>b</translation>
+    </message>
+    <message>
+        <source>MB</source>
+        <translation>MB</translation>
+    </message>
+    <message>
+        <source>%v / %m MB</source>
+        <translation>%v / %m MB</translation>
+    </message>
+    <message>
+        <source>Open image file</source>
+        <translation>Kalıp dosyası aç</translation>
+    </message>
+    <message>
+        <source>&amp;Write</source>
+        <translation>&amp;Yazdır</translation>
+    </message>
+    <message>
+        <source>Failed to open the image file:</source>
+        <translation>Kalıp dosyası açılamadı:</translation>
+    </message>
+    <message>
+        <source>Clea&amp;r</source>
+        <translation>&amp;Temizle</translation>
+    </message>
+    <message>
+        <source>Disk Images</source>
+        <translation>Disk Kalıpları</translation>
+    </message>
+    <message>
+        <source>Image:</source>
+        <translation>Kalıp:</translation>
+    </message>
+    <message>
+        <source>Refresh the list</source>
+        <translation>Listeyi yenile</translation>
+    </message>
+    <message>
+        <source>Image size:</source>
+        <translation>Kalıp boyutu:</translation>
+    </message>
+    <message>
+        <source>&amp;USB Device:</source>
+        <translation>&amp;USB Aygıt:</translation>
+    </message>
+    <message>
+        <source>Warning!</source>
+        <translation>Uyarı!</translation>
+    </message>
+    <message>
+        <source>The image is larger than your selected device!</source>
+        <translation>Görüntü seçilen cihazınızdan daha büyük!</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to proceed?</source>
+        <translation>Devam etmek istediğinizden emin misiniz?</translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;İptal</translation>
+    </message>
+    <message>
+        <source>Delete partitions and make all space available for storing data</source>
+        <translation>Bölümleri silin ve verileri depolamak için tüm alanı kullanılabilir yapın</translation>
+    </message>
+    <message>
+        <source>Writing is in progress, abort it?</source>
+        <translation>Yazma devam ediyor, iptal edilsin mi?</translation>
+    </message>
+    <message>
+        <source>Disk size:</source>
+        <translation>Disk boyutu:</translation>
+    </message>
+    <message>
+        <source>All Files</source>
+        <translation>Tüm Dosyalar</translation>
+    </message>
+    <message>
+        <source>All existing data on the selected device will be lost!</source>
+        <translation>Seçilen cihazdaki tüm mevcut veriler kaybolacak!</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>MB</source>
+        <translation>MB</translation>
+    </message>
+    <message>
+        <source>Unknown Device</source>
+        <translation>Bilinmeyen Aygıt</translation>
+    </message>
+    <message>
+        <source>Error code:</source>
+        <translation>Hata kodu:</translation>
+    </message>
+    <message>
+        <source>Error!</source>
+        <translation>Hata!</translation>
+    </message>
+    <message>
+        <source>Failed to restart with root privileges! (Error code: %1)</source>
+        <translation>Root yetkileri ile yeniden başlatılamadı! (Hata kodu: %1)</translation>
+    </message>
+    <message>
+        <source>ConnectServer failed.</source>
+        <translation>ConnectServer başarısız oldu.</translation>
+    </message>
+    <message>
+        <source>No appropriate su-application found!</source>
+        <translation>Uygun SU başvurusu bulunamadı!</translation>
+    </message>
+    <message>
+        <source>&lt;unmounted&gt;</source>
+        <translation>&lt;unmounted&gt;</translation>
+    </message>
+    <message>
+        <source>Failed to query list of partitions.</source>
+        <translation>Bölümlerin listesini sorgulama işlemi başarısız oldu.</translation>
+    </message>
+    <message>
+        <source>CoCreateInstance(WbemAdministrativeLocator) failed.</source>
+        <translation>CoCreateInstance(WbemAdministrativeLocator) hata oldu.</translation>
+    </message>
+    <message>
+        <source>Memory allocation for %1 failed.</source>
+        <translation>%1 için bellek ayırma başarısız oldu.</translation>
+    </message>
+    <message>
+        <source>Please, restart the program with root privileges.</source>
+        <translation>Lütfen programı kök ayrıcalıklarıyla yeniden başlatınız.</translation>
+    </message>
+    <message>
+        <source>Failed to query USB flash devices.</source>
+        <translation>USB bellek aygıtları sorgulanamadı.</translation>
+    </message>
+    <message>
+        <source>Failed to query list of logical disks.</source>
+        <translation>Mantıksal disklerin listesini sorgulama işlemi başarısız oldu.</translation>
+    </message>
+</context>
+<context>
+    <name>ImageWriter</name>
+    <message>
+        <source>Failed to write to the device:</source>
+        <translation>Cihaza yazma başarısız oldu:</translation>
+    </message>
+    <message>
+        <source>Failed to open the drive</source>
+        <translation>Sürücü açılamadı</translation>
+    </message>
+    <message>
+        <source>Failed to open the image file:</source>
+        <translation>Kalıp dosyası açılamadı:</translation>
+    </message>
+    <message>
+        <source>Failed to open the target device:</source>
+        <translation>Hedef cihazı açamadı:</translation>
+    </message>
+    <message>
+        <source>Now you need to format your device.</source>
+        <translation>Şimdi cihazınızı biçimlendirmeniz gerekiyor.</translation>
+    </message>
+    <message>
+        <source>The operation completed successfully.</source>
+        <translation>İşlem başarıyla tamamlandı.</translation>
+    </message>
+    <message>
+        <source>Failed to unmount the volume</source>
+        <translation>Birimin kaldırılması başarısız oldu</translation>
+    </message>
+    <message>
+        <source>Failed to read the image file:</source>
+        <translation>Kalıp dosyasını okuma başarısız oldu:</translation>
+    </message>
+    <message>
+        <source>Failed to unmount the drive</source>
+        <translation>Sürücünün bağlantısı kesilemedi</translation>
+    </message>
+    <message>
+        <source>The last block was not fully written (%1 of %2 bytes)!
+Aborting.</source>
+        <translation>Son blok tamamen yazılmadı (%2 de %1 bayt)!
+Durduruluyor.</translation>
+    </message>
+    <message>
+        <source>To be able to store data on this device again, please, use the button &quot;Clear&quot;.</source>
+        <translation>Bu cihaza tekrar veri depolamak için lütfen &quot;Temizle&quot; düğmesini kullanın.</translation>
+    </message>
+    <message>
+        <source>Failed to allocate memory for buffer:</source>
+        <translation>Arabellek için bellek ayırma başarısız:</translation>
+    </message>
+    <message>
+        <source>Failed to allocate memory for buffer.</source>
+        <translation>Arabellek için bellek ayıramadı.</translation>
+    </message>
+</context>
+<context>
+    <name>PhysicalDevice</name>
+    <message>
+        <source>Could not acquire lock:</source>
+        <translation>Kilit alınamadı:</translation>
+    </message>
+</context>
+</TS>
